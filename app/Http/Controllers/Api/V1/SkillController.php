96646883378f85a6b1218182d6aaa7690053c50f
chumardo <dachigiorgadze98@gmail.com>
2023-01-29 16:44:10 +0400

    create skill store method for add skills

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
@@ -10,5 +12,11 @@ class SkillController extends Controller
     public function index()
     {
         return response()->json("Skill Index");
+    }
+
+    public function store(StoreSkillRequest $request)
+    {
+        Skill::create($request->validated());
+        return response()->json("Skill Created!");
     }
 }
