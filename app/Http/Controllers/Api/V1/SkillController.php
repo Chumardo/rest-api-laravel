c2b1698414b0495a8ae5836b64ab51483a9cfb1d
chumardo <dachigiorgadze98@gmail.com>
2023-01-29 16:58:09 +0400

    create one skill show method and resource

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\V1\SkillResource;
 use App\Models\Skill;
-use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
     public function index()
     {
         return response()->json("Skill Index");
+    }
+
+    public function show(Skill $skill)
+    {
+        return new SkillResource($skill);
     }
 
     public function store(StoreSkillRequest $request)
