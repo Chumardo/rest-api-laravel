5ffe093f9608ac056810703db53fb2d2f8ab8e58
chumardo <dachigiorgadze98@gmail.com>
2023-01-29 18:49:06 +0400

    adding new skill store form

--- vue-rest-api/src/views/skills/SkillCreate.vue
+++ vue-rest-api/src/views/skills/SkillCreate.vue
@@ -1,1 +1,41 @@
-<template></template>
+<template>
+    <div class="mt-12">
+        <form class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md">
+            <div class="space-y-6">
+                <div class="mb-6">
+                    <label
+                        for="name"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >Name</label
+                    >
+                    <input
+                        type="text"
+                        id="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                </div>
+
+                <div class="mb-6">
+                    <label
+                        for="slug"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >Slug</label
+                    >
+                    <input
+                        type="text"
+                        id="slug"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                </div>
+
+                <div class="mt-4">
+                    <button
+                        class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded"
+                    >
+                        Store
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
