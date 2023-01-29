2db4ac01f3fa9dc88e359a6b0a8b855f4ee3af76
chumardo <dachigiorgadze98@gmail.com>
2023-01-29 18:44:25 +0400

    add create new skill button

--- vue-rest-api/src/views/skills/SkillIndex.vue
+++ vue-rest-api/src/views/skills/SkillIndex.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="mt-12">
+        <div class="flex justify-end m-2 p-2">
+            <RouterLink
+                :to="{ name: 'SkillCreate' }"
+                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded"
+                >Add new skill</RouterLink
+            >
+        </div>
         <div class="relative overflow-x-auto">
             <table
                 class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
