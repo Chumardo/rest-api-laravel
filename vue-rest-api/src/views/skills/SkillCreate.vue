2fb54a309df1c0039896a0bcf36b5c01cbf388af
chumardo <dachigiorgadze98@gmail.com>
2023-01-29 18:53:13 +0400

    store skills from frontend

--- vue-rest-api/src/views/skills/SkillCreate.vue
+++ vue-rest-api/src/views/skills/SkillCreate.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="mt-12">
-        <form class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md">
+        <form
+            class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md"
+            @submit.prevent="storeSkill(form)"
+        >
             <div class="space-y-6">
                 <div class="mb-6">
                     <label
@@ -11,6 +14,7 @@
                     <input
                         type="text"
                         id="name"
+                        v-model="form.name"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                 </div>
@@ -24,12 +28,14 @@
                     <input
                         type="text"
                         id="slug"
+                        v-model="form.slug"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                 </div>
 
                 <div class="mt-4">
                     <button
+                        type="submit"
                         class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded"
                     >
                         Store
@@ -39,3 +45,15 @@
         </form>
     </div>
 </template>
+
+<script setup>
+import { reactive } from "vue";
+import useSkills from "../../composables/skills";
+
+const { storeSkill, errors } = useSkills();
+
+const form = reactive({
+    name: "",
+    slug: "",
+});
+</script>
